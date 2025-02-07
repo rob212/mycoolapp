9a3dd42191f9e8b521e6f490831ab68a7af03fb3
Rob McBryde <robertmcbryde83@gmail.com>
2025-02-07 14:17:14 +0000

    application properties and how to use them

--- src/main/java/com/example/springboot/demo/mycoolapp/rest/FunRestController.java
+++ src/main/java/com/example/springboot/demo/mycoolapp/rest/FunRestController.java
@@ -1,15 +1,17 @@
 package com.example.springboot.demo.mycoolapp.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
 
-    // expose "/" that returns "Hello World!"
+    @Value("${coach.name}")
+    private String name;
 
     @GetMapping("/")
     public String sayHello() {
-        return "Hello World!";
+        return String.format("Hello %s", name);
     }
 }
